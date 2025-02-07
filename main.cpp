6e1babffcea8f925489b21cfa4bcaae7d4a8325c
aftabnaik19 <aftabnaik1419@gmail.com>
2025-02-07 07:49:49 +0000

    Added pistache http server and rapidJson to the project

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
-#include "models/3DArrayModel.cpp"
-using namespace std;
+// #include "rapidjson/document.h"
+#include <pistache/endpoint.h>
+#include <pistache/http.h>
+#include <pistache/router.h>
 
-int main()
-{
-  RubiksCube3dArray cube1, cube2, cube3, cube4, cube5, cube6;
-  cube1.d2();
-  cube1.printState();
-  cube1.b2();
-  cube1.printState();
-  cube1.l2();
-  cube1.printState();
-  cube1.d();
-  cube1.printState();
-  cube1.r();
-  cube1.printState();
-  cube1.bPrime();
-  cube1.printState();
+using namespace Pistache;
 
+void hello(const Rest::Request &request, Http::ResponseWriter response) {
+  response.send(Http::Code::Ok, "world!");
+}
+
+int main(int argc, char *argv[]) {
+  using namespace Rest;
+
+  Router router;   // POST/GET/etc. route handler
+  Port port(8081); // Port to listen on
+  Address addr(Ipv4::any(), port);
+  std::shared_ptr<Http::Endpoint> endpoint =
+      std::make_shared<Http::Endpoint>(addr);
+  auto opts =
+      Http::Endpoint::options().threads(1); // Number of threads for the server
+  endpoint->init(opts);
+
+  std::cout << "Server is initialized" << std::endl;
+  Routes::Get(router, "/hello", Routes::bind(&hello));
+
+  endpoint->setHandler(router.handler());
+  std::cout << "Server is running..." << std::endl;
+  endpoint->serve();
+
+  // Optionally print shutdown message after server stops.
+  std::cout << "Server is shut down" << std::endl;
   return 0;
 }
